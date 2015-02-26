80af9dfedd37dc505fe018bda2a6b9e208501808
Simon Armstrong <sta@compsoc.dur.ac.uk>
2015-02-26 13:10:20 +0000

    Fixed so moves to end on create rather than first read and added a timeout

--- screen/tail.py
+++ screen/tail.py
@@ -18,75 +18,91 @@ import time
 import string
 import os
 
-def tail(filename, tailbytes = 0, sleepfor = 5):
-    '''Examine file looking for new lines.
-       When called, this function will process all lines in the file being
-       tailed, detect the original file being renamed or reopened, etc... and
-       sleep if nothing is found. If the file can't be opened raises StopIteration
-       
-       @return Nothing
-       @param filename File to read.
-       @param tailbytes Specifies bytes from end of file to start reading
-         (defaults to 0, meaning skip entire file, -1 means read full file).
-       @param sleepfor Amount of time to sleep between attempts to read
-    '''
-    fp = None
+
+def tail(filename, tailbytes = 0, sleepfor = 2, timeout=-1):
+  '''Examine file looking for new lines.
+     When called, this function will process all lines in the file being
+     tailed, detect the original file being renamed or reopened, etc... and
+     sleep if nothing is found. If the file can't be opened raises StopIteration
+     
+     @return Nothing
+     @param filename File to read.
+     @param tailbytes Specifies bytes from end of file to start reading
+       (defaults to 0, meaning skip entire file, -1 means read full file).
+     @param sleepfor Amount of time to sleep between attempts to read
+     @param timeout Amount of time to yield after even if no new input. If the 
+       timeout triggers then we yield None
+  '''
+  try:
+    fp = open(filename, 'r')
+    stat = os.stat(filename)
+    lastInode = stat[1]
+    if tailbytes >= 0 and stat[6] > tailbytes:
+      fp.seek(0 - (tailbytes), 2)
+    return _gen(filename,fp,lastInode,sleepfor,timeout)
+  except (IOError, OSError):
+    if fp:
+      fp.close()
+    return _gen(filename,None,-1,sleepfor)
+
+def _nogen():
+  return
+  yield
+def _gen(filename, fp, lastInode, sleepfor,timeout):
     lastSize = 0
-    lastInode = -1
     data = ''
+    sleeps=0 # sleeps since last succesful yield
     while 1:
       #  open file if it's not already open
       if not fp:
         try:
           fp = open(filename, 'r')
           stat = os.stat(filename)
-          lastIno = stat[1]
-          if tailbytes >= 0 and stat[6] > tailbytes:
-            fp.seek(0 - (tailbytes), 2)
-          tailbytes = -1
-          lastSize = 0
+          lastInode = stat[1]
         except (IOError, OSError):
           if fp:
             fp.close()
-          skip = -1    #  if the file doesn't exist, we don't skip
           fp = None
       if not fp:
         raise StopIteration()
 
+      #  read any new data
+      while 1:
+        thisData = fp.read(4096)
+        if len(thisData) < 1:
+          break
+        print "read data '"+thisData+"'"
+        data = data + thisData
+        #  process lines within the data
+        while 1:
+          pos = string.find(data, '\n')
+          if pos < 0:
+            break
+          line = data[:pos]
+          data = data[pos + 1:]
+          #  line is line read from file
+          yield line
+          sleeps=1
       #  check to see if file has moved under us
       try:
         stat = os.stat(filename)
         thisSize = stat[6]
-        thisIno = stat[1]
-        if thisSize < lastSize or thisIno != lastIno:
+        thisInode = stat[1]
+        if thisSize < lastSize or thisInode != lastInode:
           raise OSError(0,"file updated")
       except OSError:
+        yield data # give rest of data as a line even if no newline if we are starting a new input file (implict \n at start of new file)
         fp.close()
         fp = None
         data = ''
         continue
-
-      #  read if size has changed
-      if lastSize < thisSize:
-        while 1:
-          thisData = fp.read(4096)
-          if len(thisData) < 1:
-            break
-          print "read data '"+thisData+"'"
-          data = data + thisData
-
-          #  process lines within the data
-          while 1:
-            pos = string.find(data, '\n')
-            if pos < 0:
-              break
-            line = data[:pos]
-            data = data[pos + 1:]
-            #  line is line read from file
-            yield line
-
       lastSize = thisSize
-      lastIno = thisIno
-      time.sleep(sleepfor)
+      lastInode = thisInode
+      if timeout>0 and sleeps*sleepfor>timeout:
+        yield None
+        sleeps=0
+      else:
+        time.sleep(sleepfor)
+        sleeps+=1
 
 __all__=["tail"]
