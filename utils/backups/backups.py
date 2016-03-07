5f1b0235a242f9a5741b860bb687a00c5a9baff5
Simon Armstrong <sta@compsoc.dur.ac.uk>
2016-03-07 14:56:01 +0000

    Fixed a few bugs

--- utils/backups/backups.py
+++ utils/backups/backups.py
@@ -21,6 +21,7 @@ then this profiles inherits settings from the named profile. If the profile has 
 specified then if 'replace_targets' is True the base profiles targets are replaced, otherwise the are combined.
 The equivelent applies with exclusions and replace_exclusions. It is not valid to have a cycle in the 'base'
 relations and if this occurs the result is unspecified.
+NOTE: Profile names can't include spaces.
 
 If a profile has a lifetime then any backups older than this amount of time are automatically deleted after
 the update.
@@ -56,7 +57,7 @@ def getprofiledata(config,profile):
       else:
         newprofiledata["targets"]=newprofiledata["targets"]+profiledata["targets"]
     if "exclusions" in profiledata:
-      if "replace_exclusions" in profiledata and profiledata["replace_exclusion"] or "exclusions" not in newprofiledata:
+      if "replace_exclusions" in profiledata and profiledata["replace_exclusions"] or "exclusions" not in newprofiledata:
         newprofiledata["exclusions"]=profiledata["exclusions"]
       else:
         newprofiledata["exclusions"]=newprofiledata["exclusions"]+profiledata["exclusions"]
@@ -67,36 +68,29 @@ def getprofiledata(config,profile):
     return profiledata
 
 def applydelta(timestamp,amount,unit):
-  if amount=0:
+  if amount == 0:
     return timestamp
   if unit.lower() == "year":
     return timestamp.replace(year=timestamp.year+amount)
   if unit.lower() =="month":
-   dy,m=divmod(timestamp.month+amount-1,12)
-   m+=1
+   year,month=divmod(timestamp.month+amount-1,12)
+   month+=1
+   year+=timestamp.year
    day = min(timestamp.day,calendar.monthrange(year,month)[1])
-   return timestamp.replace(year=timestamp.year+dy,month=m,day=day
-  if unit.lower() in ("week","day"):
-    return timestamp+timedelta(**{unit.lower()+"s":amount)
+   return timestamp.replace(year=year,month=month,day=day)
+  if unit.lower() in ["week","day"]:
+    return timestamp+datetime.timedelta(**{unit.lower()+"s":amount})
   print("ERROR: Unknown time unit. Treating as 0")
   return timestamp
 
 def doschedule(config,now,backups):
   schedule=config["schedule"]
   for tag,maxage,ageunit in schedule:
-    if tag not in backups or backups[tag][0]<applydelta(now,-maxage,ageunit):
+    if tag not in backups or backups[tag][0][1]<applydelta(now,-maxage,ageunit):
       return tag
   return tag
 
-def dobackup(dir,data,now);
-  targetname=os.path.join(BACKUPDIR,tag+" "+now.strftime(TIMESTAMPFORMAT)+".zip")
-  try:
-    sp.check_call(['zip','-ry',targetname]+data['targets']+["-x"]+data["exclusions"],cwd=server.data['dir'])
-  except sp.CalledProcessError as ex:
-    print("Error backing up the server")
-
-
-def backup(dir,config,profile=None,*):
+def backup(dir,config,profile=None):
   now=datetime.datetime.utcnow()
   backupdir
   for f in os.listdir(os.path.join(dir,BACKUPDIR)):
@@ -108,11 +102,17 @@ def backup(dir,config,profile=None,*):
       backups[tag]=[]
     backups[tag].append((f,timestamp))
   for key,val in backups:
-    val.sort(key=lambda (f,ts): ts)
+    val.sort(key=lambda ft: ft[1])
  
   if profile==None:
     profile=doschedule(config,now,backups)
-  dobackup(dir,getprofiledata(config,profile),now)
+  data=getprofiledata(config,profile)
+  targetname=os.path.join(BACKUPDIR,profile+" "+now.strftime(TIMESTAMPFORMAT)+".zip")
+  try:
+    sp.check_call(['zip','-ry',targetname]+data['targets']+["-x"]+data["exclusions"],cwd=server.data['dir'])
+  except sp.CalledProcessError as ex:
+    print("Error backing up the server")
+    return
   
   for tag,files in backups.items():
     profile=getprofiledata(config,tag)
@@ -123,5 +123,5 @@ def backup(dir,config,profile=None,*):
     for filename,timestamp in files:
       if timestamp<earliesttimestamp:
         print("Removing out of date backup: {}".format(filename))
-        os.remove(os.path.join(os.path.join(dir,BACKUPDIR),filename)
+        os.remove(os.path.join(os.path.join(dir,BACKUPDIR),filename))
 
