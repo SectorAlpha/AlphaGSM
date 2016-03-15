15a8fecd069f9b8029006b93de5e32ffa82256ed
Simon Armstrong <sta@compsoc.dur.ac.uk>
2016-03-15 19:02:12 +0000

    updated to new settings system

--- downloadermodules/steamcmd.py
+++ downloadermodules/steamcmd.py
@@ -1,7 +1,7 @@
 #todo 
 
-import settings_local
-import downloadermodules.url as urlextra
+from utils.settings import settings 
+from . import url as urlextra
 
 # check if steamcmd exists, if not download it and install it via wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
 # execute steamcmd/steamcmd.sh
@@ -9,7 +9,8 @@ import downloadermodules.url as urlextra
 # ./steamcmd +login <user> +force_install_dir <download_location> +app_update <appid> +quit
 
 def install_steamcmd():
-  install_dir = settings_local.ROOT_DIR + "steamcmd/"
+  # only run as the downloads user so ~ will be that users home directory
+  install_dir = setting.user.getsection("downloader").getsection("steamcmd").get("path",os.path.expanduser("~/steamcmd"))
   url = "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz"
-  
-  urlextra.download(path,url,"steamcmd")
+  urlextra.download(install_dir,[url,"steamcmd.tgz","tgz"])
+
