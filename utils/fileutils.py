78192d75dda97100a7dba876edadeedc57270534
Ben <bthompson2090@gmail.com>
2016-12-03 16:48:19 +0000

    added the fileutils file

---
+++ utils/fileutils.py
@@ -0,0 +1,23 @@
+import os
+import errno
+
+def make_empty_file(file_path):
+  """
+  check_exists: if the file exists, then do not make the empty file
+  """
+
+  flags = os.O_CREAT | os.O_EXCL | os.O_WRONLY
+
+  try:
+    file_handle = os.open(file_path, flags)
+  except OSError as e:
+    if e.errno == errno.EEXIST:  # Failed as the file already exists.
+        pass
+    else:  # Something unexpected went wrong so reraise the exception.
+        raise
+  else:  # No exception, so the file must have been created successfully.
+    with os.fdopen(file_handle, 'w') as file_obj:
+        # Using `os.fdopen` converts the handle to an object that acts like a
+        # regular Python file object, and the `with` context manager means the
+        # file will be automatically closed when we're done with it.
+        file_obj.write("")
