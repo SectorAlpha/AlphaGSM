eab91992383b2b8e7a21343ccbac3cf6eb3ada0c
Ben <bthompson2090@gmail.com>
2016-12-03 21:45:46 +0000

    inclided file close statements

--- utils/fileutils.py
+++ utils/fileutils.py
@@ -12,12 +12,15 @@ def make_empty_file(file_path):
     file_handle = os.open(file_path, flags)
   except OSError as e:
     if e.errno == errno.EEXIST:  # Failed as the file already exists.
+        os.close(file_handle)
         pass
     else:  # Something unexpected went wrong so reraise the exception.
-        raise
+        raise OSError
   else:  # No exception, so the file must have been created successfully.
     # Using `os.fdopen` converts the handle to an object that acts like a
     # regular Python file object, and the `with` context manager means the
     # file will be automatically closed when we're done with it.
     os.fdopen(file_handle, 'w')
+    os.close(file_handle)
+    
 
